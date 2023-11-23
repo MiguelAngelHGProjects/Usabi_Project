1a8e58681012415c9cbb4bd9473e678468fc177d
MiguelAngelHGProjects <miguelangelhernandezgarcia@alumno.ieselrincon.es>
2023-11-23 09:55:55 +0000

    basic auht added

--- backend/app/controllers/users/sessions_controller.rb
+++ backend/app/controllers/users/sessions_controller.rb
@@ -1,54 +1,61 @@
+# app/controllers/users/sessions_controller.rb
+
 class Users::SessionsController < Devise::SessionsController
   include RackSessionFix
+  include ActionController::HttpAuthentication::Basic
   respond_to :json
-  before_action :authenticate_user!, only: [:destroy] 
-  # Override the set_flash_message method with an empty implementation
-  def set_flash_message(key, kind, options = {})
-  end
-
-  # Override the set_flash_message! method with an empty implementation
-  def set_flash_message!(key, kind, options = {})
-  end
+  before_action :authenticate_user!, only: [:destroy]
 
   def create
-    # debugger
-    self.resource = warden.authenticate!(auth_options)
-    sign_in(resource_name, resource)
+    user = User.find_by(email: params[:user][:email])
 
-    respond_with(resource, location: after_sign_in_path_for(resource))
+    if user && user.valid_password?(params[:user][:password])
+      sign_in(user)
+      render json: {
+        status: 'success',
+        data: UserSerializer.new(user).as_json,
+        token: request.env['warden-jwt_auth.token']
+      }
+    else
+      render json: {
+        status: 'error',
+        message: 'Invalid credentials'
+      }, status: :unauthorized
+    end
   end
 
-def destroy
-  if user_signed_in?
-    sign_out(current_user)
-    render json: {
-      status: { code: 200, message: 'Logged out successfully.' }
-    }, status: :ok
-  else
-    render json: {
-      status: 401,
-      message: "Couldn't find an active session."
-    }, status: :unauthorized
+  def destroy
+    if user_signed_in?
+      sign_out(current_user)
+      render json: {
+        status: 'success',
+        message: 'Logged out successfully.'
+      }
+    else
+      render json: {
+        status: 'error',
+        message: 'Couldn\'t find an active session.'
+      }, status: :unauthorized
+    end
   end
-end
 
   private
 
   def respond_with(resource, _opts = {})
-  if resource.present?
-    render json: {
-      status: { code: 200, message: 'Logged in successfully.' },
-      data: UserSerializer.new(resource).as_json,
-      token: request.env["warden-jwt_auth.token"],
-    }, status: :ok
-  else
-    render json: {
-      status: 404,
-      error: "Not Found",
-      message: "User not found."
-    }, status: :not_found
+    if resource.present?
+      render json: {
+        status: { code: 200, message: 'Logged in successfully.' },
+        data: UserSerializer.new(resource).as_json,
+        token: request.env["warden-jwt_auth.token"],
+      }, status: :ok
+    else
+      render json: {
+        status: 404,
+        error: "Not Found",
+        message: "User not found."
+      }, status: :not_found
+    end
   end
-end
 
   def respond_to_on_destroy
     if current_user
--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -4,8 +4,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource(
       '*',
       headers: :any,
-      expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
-      methods: [:get, :patch, :put, :delete, :post, :options, :show]
+      methods: [:get, :patch, :put, :delete, :post, :options, :show],
+      credentials: true,
+      expose: ['access-token', 'expiry', 'token-type', 'Authorization']
     )
   end
 end
