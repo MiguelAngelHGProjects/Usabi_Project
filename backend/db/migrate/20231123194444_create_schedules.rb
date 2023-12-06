057c5ae411a65985d3f2776786d20b032774d3af
MiguelAngelHGProjects <miguelangelhernandezgarcia@alumno.ieselrincon.es>
2023-12-06 09:52:54 +0000

    change password added.

--- backend/db/migrate/20231123194444_create_schedules.rb
+++ backend/db/migrate/20231123194444_create_schedules.rb
@@ -1,7 +1,7 @@
 class CreateSchedules < ActiveRecord::Migration[7.1]
   def change
     create_table :schedules do |t|
-      t.integer :ProjectId
+      t.references :project, foreign_key: true
       t.string :Room
       t.string :ScheduleType
       t.string :ScheduleName
